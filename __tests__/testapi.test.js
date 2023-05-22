e214abeb5b80805d51a57412d2c3dd2453b0852c
ILoveGameCodng <sonkim1001@naver.com>
2023-05-22 15:57:12 +0900

    test: Add test codes for testapi1

---
+++ __tests__/testapi.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const { app, server } = require('../app.js')
+
+describe('app test', () => {
+    afterAll(() => server.close())
+    
+    test('testapi1 api', (done) => {
+        request(app).get('/testapi1').expect(200).end((err, res) => {
+            if (err) throw err
+            expect(res.body).toMatchObject({
+                result: true,
+                message: 'Test API 1'
+            })
+            done()
+        })
+    })
+})
