5d17cb7c10f098f92e0eab82ceddaba3b677c8f9
ILoveGameCodng <sonkim1001@naver.com>
2023-05-22 16:10:15 +0900

    refactor: Change test result to false

--- __tests__/testapi.test.js
+++ __tests__/testapi.test.js
@@ -3,12 +3,12 @@ const { app, server } = require('../app.js')
 
 describe('app test', () => {
     afterAll(() => server.close())
-    
+
     test('testapi1 api', (done) => {
         request(app).get('/testapi1').expect(200).end((err, res) => {
             if (err) throw err
             expect(res.body).toMatchObject({
-                result: true,
+                result: false,
                 message: 'Test API 1'
             })
             done()
